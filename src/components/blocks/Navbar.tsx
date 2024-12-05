3d53ee288d33237a906d488058c50a645d4b0a2f
Maksym <maxyach6@gmail.com>
2024-12-05 21:41:58 +0200

    Attempt to fix icons

--- src/components/blocks/Navbar.tsx
+++ src/components/blocks/Navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
                 setTheme("dark");
               }}
             >
-              <SunIcon className="size-4 bg-foreground"/>
+              <SunIcon id="SunIcon" className="size-4 bg-foreground"/>
             </button>
           )}
           {theme === "dark" && (
@@ -53,7 +53,7 @@ export default function Navbar() {
                 setTheme("light");
               }}
             >
-              <MoonStarIcon className="size-4 bg-foreground" />
+              <MoonStarIcon id="MoonStarIcon" className="size-4 bg-foreground" />
             </button>
           )}
         </motion.li>
