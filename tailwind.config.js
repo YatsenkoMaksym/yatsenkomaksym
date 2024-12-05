3210409d0708da822633b036b05e09ae98f358dc
Maksym <maxyach6@gmail.com>
2024-12-05 21:05:11 +0200

    Fix tailwind.config.js for Vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
-import tlwnd from "tailwindcss-animate";
+
+const tailwindcssAnimate = require('tailwindcss-animate');
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -87,5 +89,5 @@ module.exports = {
       },
     },
   },
-  plugins: [tlwnd],
+  plugins: [tailwindcssAnimate],
 };
