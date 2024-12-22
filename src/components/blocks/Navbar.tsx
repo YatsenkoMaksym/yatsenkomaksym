76d5c40e87ffeedd5fb9b879fb5e1a6759168c2a
Maksym Yatsenko <maxyach6@gmail.com>
2024-12-22 23:23:10 +0200

    Some small details about navbar

--- src/components/blocks/Navbar.tsx
+++ src/components/blocks/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
   };
 
   return (
-    <header className="sticky top-0 z-50 w-full flex justify-center">
+    <header className="sticky top-10 z-50 w-full flex justify-center">
       <nav className="outliner z-50 w-1/2 min-w-fit place-items-center overflow-hidden rounded-2xl bg-background px-8 py-5 opacity-80 sm:w-1/3">
         <motion.ul className="flex flex-row gap-16">
           <motion.li variants={liVariants} whileHover="hover" whileTap="tap">
